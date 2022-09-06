1a58c1c1650e7833cb02f284ada360e7095050f8
Rajarshi Banerjee <rajarshiban13@gmail.com>
2022-09-07 01:09:06 +0530

    adding ann viz

---
+++ viz-nn/main.py
@@ -0,0 +1,170 @@
+from tkinter import *
+from ttkbootstrap import *
+import canvasvg,random
+
+
+
+COLORS = ['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white', 'old lace',
+          'linen', 'antique white', 'papaya whip', 'blanched almond', 'bisque', 'peach puff',
+          'navajo white', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
+          'lavender blush', 'misty rose', 
+          'light slate gray', 'gray', 'light grey', 'midnight blue', 'navy', 'cornflower blue', 'dark slate blue',
+          'slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue',  'blue',
+          'dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+          'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise',
+          'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green',
+          'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green', 'spring green',
+          'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green',
+          'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow',
+          'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown',
+          'indian red', 'saddle brown', 'sandy brown',
+          'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange',
+          'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink',
+          'pale violet red', 'maroon', 'medium violet red', 'violet red',
+          'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
+          'thistle', 'snow2', 'snow3',
+          'snow4', 'seashell2', 'seashell3', 'seashell4', 'AntiqueWhite1', 'AntiqueWhite2',
+          'AntiqueWhite3', 'AntiqueWhite4', 'bisque2', 'bisque3', 'bisque4', 'PeachPuff2',
+          'PeachPuff3', 'PeachPuff4', 'NavajoWhite2', 'NavajoWhite3', 'NavajoWhite4',
+          'LemonChiffon2', 'LemonChiffon3', 'LemonChiffon4', 'cornsilk2', 'cornsilk3',
+          'cornsilk4', 'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3', 'honeydew4',
+          'LavenderBlush2', 'LavenderBlush3', 'LavenderBlush4', 'MistyRose2', 'MistyRose3',
+          'MistyRose4', 'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3',
+          'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4',
+          'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
+          'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4',
+          'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4', 'LightSkyBlue1', 'LightSkyBlue2',
+          'LightSkyBlue3', 'LightSkyBlue4', 'SlateGray1', 'SlateGray2', 'SlateGray3',
+          'SlateGray4', 'LightSteelBlue1', 'LightSteelBlue2', 'LightSteelBlue3',
+          'LightSteelBlue4', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4',
+          'LightCyan2', 'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2',
+          'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2', 'CadetBlue3',
+          'CadetBlue4', 'turquoise1', 'turquoise2', 'turquoise3', 'turquoise4', 'cyan2', 'cyan3',
+          'cyan4', 'DarkSlateGray1', 'DarkSlateGray2', 'DarkSlateGray3', 'DarkSlateGray4',
+          'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2', 'DarkSeaGreen3',
+          'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2', 'SeaGreen3', 'PaleGreen1', 'PaleGreen2',
+          'PaleGreen3', 'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4',
+          'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3', 'chartreuse4',
+          'OliveDrab1', 'OliveDrab2', 'OliveDrab4', 'DarkOliveGreen1', 'DarkOliveGreen2',
+          'DarkOliveGreen3', 'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4',
+          'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3', 'LightGoldenrod4',
+          'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4',
+          'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4',
+          'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4',
+          'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
+          'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1',
+          'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
+          'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
+          'firebrick3', 'firebrick4', 'brown1', 'brown2', 'brown3', 'brown4', 'salmon1', 'salmon2',
+          'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
+          'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',
+          'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
+          'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4',
+          'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+          'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',
+          'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',
+          'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+          'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1',
+          'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
+          'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4',
+          'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2',
+          'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4',
+          'gray1', 'gray2']
+
+class window():
+    def __init__(self,root):
+        self.root = root
+        self.root.title("ANN Visualizer")
+        self.root.resizable(width=False, height=False)
+
+        self.nam = StringVar()
+
+        ttk.Entry(self.root,textvariable=self.nam).pack(fill=X)
+        self.nam.set('5-4-2')
+
+        frame = ttk.Frame(self.root)
+        frame.pack()
+        self.colors=COLORS
+        self.b1 = ttk.Button(frame,text='RANDOM COLORS',width=100,command=self.randcolors)
+
+        self.b1.pack(side=RIGHT)
+        self.b2=ttk.Button(frame,text='ONLY WHITE',width=100,command=self.onlywhite, style="info.TButton")
+
+        self.b2.pack(side=LEFT)
+        self.canvas = Canvas(self.root,width=1200,height=720,bg='white')
+
+        self.canvas.pack(side=LEFT)
+        self.scrollbar = ttk.Scrollbar(self.root,orient=VERTICAL,command = self.canvas.yview)
+        self.scrollbar.pack(side=RIGHT,fill=Y)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind('<Configure>',self.__resetscroll)
+        
+        dosframe = ttk.Frame(self.canvas)
+        self.canvas.create_window((0,0),window=dosframe,anchor='nw')
+        self.root.bind("<Escape>", lambda a: self.__clear())
+        self.root.bind("<Return>", lambda a: self.__drawNode())
+        self.root.bind("<Control-s>", lambda a: self.__save())
+
+        self.canvas.bind("<MouseWheel>", self.__mousewheel)
+        self.canvas.configure(bg='black')
+
+    def onlywhite(self):
+        self.colors=['white','snow']
+        self.__drawNode()
+
+    def randcolors(self):
+        self.colors=COLORS
+        self.__drawNode()
+
+    def __resetscroll(self,e):
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def __mousewheel(self, event):
+        self.canvas.yview_scroll(-1*(event.delta//120), "units")
+
+    def __clear(self):
+        self.canvas.delete('all')
+
+    def __drawNode(self):
+        GAP=20
+        scalex=100
+        scaley=50
+        self.__clear()
+        l = list(map(int,self.nam.get().split('-')))
+
+        l2 = [[] for _ in range(len(l))]
+  
+        l3=[abs(i-max([GAP*i for i in l])) for i in [GAP*i for i in l]]
+        x=y=5
+        # for drawing each layer with respective number of nodes
+        for layer,node in enumerate(l):
+            for i in range(node):
+                rong=random.choice(self.colors)
+                # rongs.append(rong)
+                self.canvas.create_oval(layer*scalex + x,i*scaley + y+l3[layer],layer*scalex+GAP + x,i*scaley+GAP + y+l3[layer],
+                                            fill=rong)
+                l2[layer].append([layer*scalex + x,i*scaley + y+l3[layer],rong,layer*scalex+GAP + x,i*scaley+GAP + y+l3[layer]])
+            # for drop in range(int(node*0.2)):
+            #     l2[layer][random.randint(0,node-1)][2]='black'
+
+        
+        # for the connections between the nodes
+        for k in range(len(l2)-1):
+            for i in l2[k]:
+                for j in l2[k+1]:
+                    self.canvas.create_line(i[-2],i[-1]-GAP/2,j[0],j[1]+GAP/2,width=1,fill=i[2])
+
+        self.__resetscroll(self)
+
+    def __save(self):
+        name=self.nam.get()
+        canvasvg.saveall(name+'.svg',self.canvas)
+        
+
+
+
+
+if __name__ == '__main__':
+    win=Style(theme='cyborg').master
+    app=window(win)
+    win.mainloop()
